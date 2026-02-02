66f850a411d88329e20a9db30baa3b68d73d6905
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2026-02-02 18:39:36 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000124636_SebastianBenaya/week01/GameStore.kt
+++ src/oop_00000124636_SebastianBenaya/week01/GameStore.kt
@@ -4,3 +4,11 @@ fun main(){
     val gameTitle: String
     val price: Int
 }
+
+fun calculateDiscount(price: Int): Int{
+    if(price > 500000) {
+        return price - (2 / 10 * price)
+    } else {
+        return price - (1 / 10 * price)
+    }
+}

666a69b023f49d1f6b74a9e39d8dff71820310a7
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2026-02-02 16:11:27 +0700

    week01: implement grading logic when using expression

--- src/oop_00000124636_SebastianBenaya/week01/GradingSystem.kt
+++ src/oop_00000124636_SebastianBenaya/week01/GradingSystem.kt
@@ -4,4 +4,11 @@ fun main() {
     val name = "John Thor"
     val score = 85
     println("Nama: $name, Nilai: $score")
+    val grade = when (score) {
+        in 90..100 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        else -> "D"
+    }
+    println("Grade kamu: $grade")
 }

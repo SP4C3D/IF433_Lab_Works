4e5eed6a96ac24eff0babaf27163a2a3217f63fa
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2026-02-09 15:46:37 +0700

    week02: (task 1) create Loan class with default args

--- src/oop_00000124636_SebastianBenaya/week02/tugasmandiri/Loan.kt
+++ src/oop_00000124636_SebastianBenaya/week02/tugasmandiri/Loan.kt
@@ -1,6 +1,6 @@
 package oop_00000124636_SebastianBenaya.week02.tugasmandiri
 
-class Loan(val bookTitle: String, val borrower: String, val loanDuration: Int = 1){
+class Loan(val bookTitle: String, val borrower: String, var loanDuration: Int = 1){
     fun calculateFine(){
         if(loanDuration > 3){
             val denda: Int = (loanDuration - 3) * 2000

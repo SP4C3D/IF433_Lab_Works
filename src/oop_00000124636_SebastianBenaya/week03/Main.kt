2f4fa44cb2fb8332ea0a49d687f480aca2862f2e
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2026-02-16 14:32:37 +0700

    week03: cleanup code

--- src/oop_00000124636_SebastianBenaya/week03/Main.kt
+++ src/oop_00000124636_SebastianBenaya/week03/Main.kt
@@ -2,12 +2,10 @@ package oop_00000124636_SebastianBenaya.week03
 
 fun main(){
     val e = Employee("Budi")
-    e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
 
     e.increasePerformance()
-    e.performanceRating = 5
 
     println("Pajak yang harus dibayar: ${e.tax}")
 }

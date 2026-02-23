1d4f6d78a245c7eafa866e9d1282a5ec1952bf8d
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2026-02-23 15:23:07 +0700

    week04: (task 1) test ElectricCar in main

--- src/oop_00000124636_SebastianBenaya/week04/Main.kt
+++ src/oop_00000124636_SebastianBenaya/week04/Main.kt
@@ -1,5 +1,7 @@
 package oop_00000124636_SebastianBenaya.week04
 
+import oop_00000124636_SebastianBenaya.week04.TugasMandiri1.ElectricCar
+
 fun main() {
     println("--- Testing Vehicle ---")
     val generalVehicle = Vehicle("Sepeda Onthel")
@@ -11,4 +13,11 @@ fun main() {
     myCar.openTrunk()
     myCar.honk()
     myCar.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar2 = ElectricCar("Hyundai", 4, 67)
+    myCar2.accelerate()
+    myCar2.honk()
+    myCar2.openTrunk()
+
 }

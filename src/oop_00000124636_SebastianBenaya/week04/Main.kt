374ec0c54b446eb305fb8cb26049266cf61fa14e
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2026-02-23 15:59:41 +0700

    week04: (task 2) test Employee hierarchy in main

--- src/oop_00000124636_SebastianBenaya/week04/Main.kt
+++ src/oop_00000124636_SebastianBenaya/week04/Main.kt
@@ -1,6 +1,9 @@
 package oop_00000124636_SebastianBenaya.week04
 
 import oop_00000124636_SebastianBenaya.week04.TugasMandiri1.ElectricCar
+import oop_00000124636_SebastianBenaya.week04.TugasMandiri2.Employee
+import oop_00000124636_SebastianBenaya.week04.TugasMandiri2.Manager
+import oop_00000124636_SebastianBenaya.week04.TugasMandiri2.Developer
 
 fun main() {
     println("--- Testing Vehicle ---")
@@ -20,4 +23,13 @@ fun main() {
     myCar2.honk()
     myCar2.openTrunk()
 
+    println("\n--- Bekerja Bekerja Bekerja ---")
+    val m1 = Manager("Josiah", 20000000)
+    m1.work()
+    println("Bonus yang didapat ${m1.name} adalah Rp. ${m1.calculateBonus()}")
+
+    println("\n--- Bekerja Bekerja Bekerja ---")
+    val d1 = Developer("Ben", 15670000, "Python")
+    d1.work()
+    println("Bonus yang didapat ${d1.name} adalah Rp. ${d1.calculateBonus()}")
 }

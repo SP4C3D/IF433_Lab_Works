8211d8a152ad37c28c0f9d21ef26a4956e1f54ec
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2026-03-02 15:57:13 +0700

    week05: (task 1) test MathHelper overloading in main

---
+++ src/oop_00000124636_SebastianBenaya/week05/TugasMandiri1/MathHelper.kt
@@ -0,0 +1,7 @@
+package oop_00000124636_SebastianBenaya.week05.TugasMandiri1
+
+class MathHelper {
+    fun hitungLuas(sisi: Int): Int {return sisi * sisi}
+    fun hitungLuas(panjang: Int, lebar: Int): Int {return panjang * lebar}
+    fun hitungLuas(jariJari: Double): Double {return 3.14 * jariJari * jariJari}
+}

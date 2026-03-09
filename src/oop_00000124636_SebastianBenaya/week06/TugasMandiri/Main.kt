b1db987a823cb6b66bd4233cdff3c8f4b2280f10
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2026-03-09 16:38:13 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_00000124636_SebastianBenaya/week06/TugasMandiri/Main.kt
@@ -0,0 +1,7 @@
+package oop_00000124636_SebastianBenaya.week06.TugasMandiri
+
+fun main(){
+    val sl1 = SmartLamp("1", "Ruang Tamu")
+    val ss1 = SmartSpeaker("1", "Google Nest Dapur")
+    val cctv1 = SmartCCTV("1", "Ezviz Garasi")
+}

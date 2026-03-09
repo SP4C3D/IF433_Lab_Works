5d489adbc748458b5d65a3fa8d23ed750627ffd3
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2026-03-09 16:34:54 +0700

    week06: (task) implement advanced smart casting in Hub

--- src/oop_00000124636_SebastianBenaya/week06/TugasMandiri/SmartHomeHub.kt
+++ src/oop_00000124636_SebastianBenaya/week06/TugasMandiri/SmartHomeHub.kt
@@ -8,4 +8,11 @@ class SmartHomeHub (){
             if(device is Switchable) {device.turnOff()}
         }
     }
+
+    fun activateSecurityMode() {
+        for (device in devices) {
+            if(device is Recordable) {device.startRecord()}
+            if(device is SmartSpeaker) {device.playMusic("Sirine Peringatan")}
+        }
+    }
 }

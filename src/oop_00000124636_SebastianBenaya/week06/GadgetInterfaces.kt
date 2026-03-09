096e554406c9f0cd4bbccb2197fb7da2b191868b
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2026-03-09 15:23:39 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000124636_SebastianBenaya/week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_00000124636_SebastianBenaya.week06
+
+interface Camera {
+    fun turnOn() {println("Lensa kamera terbuka dan sensor aktif.")}
+}
+
+interface Phone {
+    fun turnOn() {println("Sinyal seluler mencari jaringan.")}
+}

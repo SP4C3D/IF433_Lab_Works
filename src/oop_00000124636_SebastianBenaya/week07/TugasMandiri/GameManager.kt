ce0bb236dc8f718f54ec1f71acce3ad3092c932e
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2026-03-17 08:29:57 +0700

    week07: (task) create GameManager singleton

---
+++ src/oop_00000124636_SebastianBenaya/week07/TugasMandiri/GameManager.kt
@@ -0,0 +1,14 @@
+package oop_00000124636_SebastianBenaya.week07.TugasMandiri
+
+object GameManager {
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+}

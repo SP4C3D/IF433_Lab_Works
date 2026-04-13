39279d2cd0c08f0cfe7b7f42e7fae3a21dcdcd4b
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2026-04-13 18:00:22 +0700

    week08: (task) implement robust ApiParser using Elvis and requireNotNull

---
+++ src/oop_00000124636_SebastianBenaya/week08/TugasMandiri/ApiParser.kt
@@ -0,0 +1,27 @@
+package oop_00000124636_SebastianBenaya.week08.TugasMandiri
+
+class ApiParser {
+    fun parseProduct(rawJson: Map<String, Any?>): Product?{
+        try {
+            val id = requireNotNull(rawJson["id"]) {"API Invalid: Missing ID"}
+            val name = requireNotNull(rawJson["name"]) {"API Invalid: Missing Name"}
+
+            val type = rawJson["type"] as? String
+
+            return when (type) {
+                "ELECTRONIC" -> {
+                    val warranty = rawJson["warranty"] as? Int ?: 12
+                    Electronic(id as String, name as String, warranty)
+                }
+                "CLOTHING" -> {
+                    val size = rawJson["size"] as? String ?: "All Size"
+                    Clothing(id as String, name as String, size)
+                }
+                else -> null
+            }
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            return null
+        }
+    }
+}

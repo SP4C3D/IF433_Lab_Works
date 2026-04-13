a3c91e25ab155166a8f116ef1a26acf20a90bd68
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2026-04-13 18:03:17 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000124636_SebastianBenaya/week08/TugasMandiri/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000124636_SebastianBenaya.week08.TugasMandiri;
+
+public class JavaPaymentService {
+    public static String processPayment (String productId){
+        return "TRX-" +productId + "-SUCCESS";
+    }
+}

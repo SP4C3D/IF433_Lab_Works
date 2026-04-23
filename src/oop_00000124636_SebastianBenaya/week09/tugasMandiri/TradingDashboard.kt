f6be889648ecb62d779385565741db4f7a17696c
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2026-04-23 20:21:56 +0700

    week09: (task) chain sortedByDescending and map for top performers

--- src/oop_00000124636_SebastianBenaya/week09/tugasMandiri/TradingDashboard.kt
+++ src/oop_00000124636_SebastianBenaya/week09/tugasMandiri/TradingDashboard.kt
@@ -13,4 +13,6 @@ fun main(){
     val closedTrades = tradeHistory.filter {it.status == "CLOSED"}
     val winningTrades = closedTrades.filter {it.roe > 0}
     val losingTrades = closedTrades.filter {it.roe <= 0}
+    val sortedWins = winningTrades.sortedByDescending { it.roe }
+    val topPerformersString = sortedWins.map{"WIN [${it.pair} - ${it.position}]: +${it.roe}% ROE (Lev: ${it.leverage}x)"}
 }

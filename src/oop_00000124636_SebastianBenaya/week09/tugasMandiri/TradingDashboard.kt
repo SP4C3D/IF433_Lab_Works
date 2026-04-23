d8c94d513b1f10e46fdd1a78483db4f7ba5b5fb4
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2026-04-23 19:57:27 +0700

    week09: populate mock trade history list

---
+++ src/oop_00000124636_SebastianBenaya/week09/tugasMandiri/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_00000124636_SebastianBenaya.week09.tugasMandiri
+
+fun main(){
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "Long", 20, 15.5, "OPEN"),
+        TradeLog("ETHUSDT", "Short", 15, -10.3, "CLOSED"),
+        TradeLog("SOLUSDT", "Long", 5, 7.3, "OPEN"),
+        TradeLog("DOGEUSDT", "Short", 100, -1.3, "CLOSED"),
+        TradeLog("SWTIDRT", "Long", 19, -19.0, "CLOSED"),
+        TradeLog("OOPIDRT", "Short", 3, 17.6, "OPEN")
+    )
+}

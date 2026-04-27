e1240e18add5407372762690f3ff6d83a9f7aeab
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2026-04-27 15:04:16 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_00000124636_SebastianBenaya/week10/tugasMandiri/CryptoDashboard.kt
+++ src/oop_00000124636_SebastianBenaya/week10/tugasMandiri/CryptoDashboard.kt
@@ -5,4 +5,5 @@ fun main(){
     coinRepo.add(Coin("BTC", 67.0))
     coinRepo.add(Coin("SWT", 19.0))
     coinRepo.add(Coin("SOL", 69.0))
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 }

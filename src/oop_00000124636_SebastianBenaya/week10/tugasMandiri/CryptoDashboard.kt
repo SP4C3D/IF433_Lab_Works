7682965b6c1a45f4ff2a8e582cd17b4bedf24600
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2026-04-27 14:56:51 +0700

    week10: (task) populate coin repository

--- src/oop_00000124636_SebastianBenaya/week10/tugasMandiri/CryptoDashboard.kt
+++ src/oop_00000124636_SebastianBenaya/week10/tugasMandiri/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_00000124636_SebastianBenaya.week10.tugasMandiri
 
 fun main(){
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 67.0))
+    coinRepo.add(Coin("SWT", 19.0))
+    coinRepo.add(Coin("SOL", 69.0))
 }

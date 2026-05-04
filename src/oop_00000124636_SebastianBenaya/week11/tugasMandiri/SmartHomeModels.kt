65af007f72bf7e78de70f46bc772000058a077fc
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2026-05-04 13:26:51 +0700

    week11: (task) create extension function for SmartDevice

--- src/oop_00000124636_SebastianBenaya/week11/tugasMandiri/SmartHomeModels.kt
+++ src/oop_00000124636_SebastianBenaya/week11/tugasMandiri/SmartHomeModels.kt
@@ -1,3 +1,7 @@
 package oop_00000124636_SebastianBenaya.week11.tugasMandiri
 
 data class SmartDevice(var name: String, var category: String, var isOnline: Boolean = false, var powerLoad: Int = 0)
+
+fun SmartDevice.diagnose(): String {
+    return "[DIAGNOSTIK] $name | Kategori: $category | Status: ${if (isOnline) "Online" else "Offline"} | Daya: $powerLoad Watt"
+}

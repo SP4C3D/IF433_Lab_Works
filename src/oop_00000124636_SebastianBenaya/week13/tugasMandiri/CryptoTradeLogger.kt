b06e714a22d7dc17c59555e14092183c06d49d0f
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2026-05-18 14:39:37 +0700

    week13: (task) implement fromCsvTrade with split

--- src/oop_00000124636_SebastianBenaya/week13/tugasMandiri/CryptoTradeLogger.kt
+++ src/oop_00000124636_SebastianBenaya/week13/tugasMandiri/CryptoTradeLogger.kt
@@ -1,5 +1,17 @@
 package oop_00000124636_SebastianBenaya.week13.tugasMandiri
 
+import oop_00000124636_SebastianBenaya.week13.Student
+
 data class TradeRecord(val id: Int, val symbol: String, val type: String, val margin: Double, val pnl: Double)
 
 fun TradeRecord.toCsv(): String = "$id, $symbol, $type, $margin, $pnl"
+
+fun fromCsvTrade(line: String): TradeRecord? {
+    val parts = line.split(",")
+    return try {
+        TradeRecord(parts[0].toInt(), parts[1], parts[2], parts[3].toDouble(), parts[4].toDouble())
+    } catch (e: Exception) {
+        println("(Log) Error: $line")
+        null
+    }
+}

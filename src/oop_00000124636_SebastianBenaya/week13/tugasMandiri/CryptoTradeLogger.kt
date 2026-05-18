54c9bade7376553d468b6810e100f6eecb74c28a
Sebastian Benaya <98514080+SP4C3D@users.noreply.github.com>
2026-05-18 14:31:07 +0700

    week13: (task) implement toCsv for TradeRecord

--- src/oop_00000124636_SebastianBenaya/week13/tugasMandiri/CryptoTradeLogger.kt
+++ src/oop_00000124636_SebastianBenaya/week13/tugasMandiri/CryptoTradeLogger.kt
@@ -1,3 +1,5 @@
 package oop_00000124636_SebastianBenaya.week13.tugasMandiri
 
 data class TradeRecord(val id: Int, val symbol: String, val type: String, val margin: Double, val pnl: Double)
+
+fun TradeRecord.toCsv(): String = "$id, $symbol, $type, $margin, $pnl"
